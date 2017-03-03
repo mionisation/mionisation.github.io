76dc1eaa3537d7778447aec081dc9d491c8f89d1
Michael Ion <michael.antonius.ion@gmail.com>
2017-03-03 12:52:15 +0100

    Add generic crossword

---
+++ Crossword/js/script.js
@@ -0,0 +1,30 @@
+// A javascript-enhanced crossword puzzle [c] Jesse Weisbeck, MIT/GPL 
+(function($) {
+	$(function() {
+		// provide crossword entries in an array of objects like the following example
+		// Position refers to the numerical order of an entry. Each position can have 
+		// two entries: an across entry and a down entry
+		var puzzleData = [
+			 	{
+					clue: "First letter of greek alphabet",
+					answer: "alpha",
+					position: 1,
+					orientation: "across",
+					startx: 1,
+					starty: 1
+				},
+				{
+					clue: "Defines the alpha-numeric characters that are typically associated with text used in programming",
+					answer: "ascii",
+					position: 1,
+					orientation: "down",
+					startx: 1,
+					starty: 1
+				}
+			] 
+	
+		$('#puzzle-wrapper').crossword(puzzleData);
+		
+	})
+	
+})(jQuery)
